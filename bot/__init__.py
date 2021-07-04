d920c025a1d756c55bb0ca4698bad8045360e8a0
rihu-5 <86797300+rihu-5@users.noreply.github.com>
2021-07-04 19:24:59 +0530

    __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -73,7 +73,11 @@ DEFAULT_START_TEXT = (
 "Globally muted: No\n"
 "Thanks for using me"
 )
-# /start message when other users start your bot
+
+START_IMG = os.environ.get('START_IMG', None)if START_IMG is None: img = "https://telegra.ph/file/fc734b227985a1524e715.jpg")
+
+ 
+/start message when other users start your bot
 START_OTHER_USERS_TEXT = int(get_config(
     "START_OTHER_USERS_TEXT",
     0
